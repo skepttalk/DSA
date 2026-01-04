68747ac9ee7102fa09cf41c4c1b640e306224e3a
skepttalk <kuldeepsingh861915@gmail.com>
2026-01-04 20:04:21 +0530

    chore:code

--- BuySell.js
+++ BuySell.js
@@ -1,10 +1,33 @@
-var maxProfit = function(prices) {
-    let min = Infinity;
-    let profit = 0;
+// var maxProfit = function(prices) {
+//     let min = Infinity;
+//     let profit = 0;
 
-    for (let price of prices) {
-        min = Math.min(min, price);
-        profit = Math.max(profit, price - min);
-    }
-    return profit;
-};
+//     for (let price of prices) {
+//         min = Math.min(min, price);
+//         profit = Math.max(profit, price - min);
+//     }
+//     return profit;
+// };
+// write me output of sum of array 
+
+
+
+function anagram (a1,a2){
+    
+    a1 = a1.split('').sort().join('')
+    a2 = a2.split('').sort().join('')
+    return a1 === a2;
+
+}
+
+const a1 = "file";
+const a2 = "life";
+
+if(anagram(a1,a2)){
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+
+ 
